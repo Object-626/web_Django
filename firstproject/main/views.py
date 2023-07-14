ae88edf9ad927e9eb439e4e06daab0ebe9d2cd78
Object626 <Shtokov6161@gmail.com>
2023-07-14 20:57:48 +0400

    попробовал повыводить данные: списки, словари. поприменял фильтры

--- firstproject/main/views.py
+++ firstproject/main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', '123'],
+        'obj': {
+            'car': 'No',
+            'age': 23,
+            'hobby': 'Armwrestling'
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
