1d6ef29ebadfd8f7d06b8cb3de77cb875f9e5ba5
Object626 <Shtokov6161@gmail.com>
2023-07-16 03:54:19 +0400

    написал и вывел статьи в разделе "новости"(не работают стили для новостей)

---
+++ firstproject/news/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .models import Artiles
+def news_home(request):
+    news = Artiles.objects.all()
+    return render(request, 'news/news_home.html', {'news': news})
