3392d7d476d7b3836b48a41ecc56f1923c048f78
Object626 <Shtokov6161@gmail.com>
2023-07-17 03:34:11 +0400

    стили подтянулись, очистил кэш и обновил страницу. Создал форму для добавления записей, настроил внешний вид и функционал.

--- firstproject/news/views.py
+++ firstproject/news/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Artiles
+from .forms import ArtilesForm
 def news_home(request):
     news = Artiles.objects.all()
     return render(request, 'news/news_home.html', {'news': news})
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArtilesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = ArtilesForm()
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'news/create.html', data)
